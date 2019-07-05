715e3e87906fc2dbc4d72a627cf429530ec474bc
Robin Lovelace <rob00x@gmail.com>
2019-07-05 15:48:09 +0100

    Add inst files

---
+++ R/runUpthat.R
@@ -0,0 +1,10 @@
+#' launches the Urban Planning and Transport Health Assessment Tool app
+#'
+# wrapper for shiny::shinyApp()
+#' @return shiny application object
+#'
+#' @import shiny
+#' @export
+runUpthat <- function() {
+  shinyApp(ui = shinyAppUI, server = shinyAppServer)
+}
