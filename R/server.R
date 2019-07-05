17fc7c000e335c456be275a0f0550dea88f5758d
Robin Lovelace <rob00x@gmail.com>
2019-07-05 21:39:28 +0100

    Add colourscheme

--- R/server.R
+++ R/server.R
@@ -10,10 +10,20 @@ shinyAppServer <- function(input, output, session) {
   net = sf::read_sf("net.geojson")
   n = names(net)
   n_flow = n[grepl(pattern = "flow_", x = n)]
+  
+  
+  # map %>%
+  #   addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 1,
+  #               color = ~qpal(gdp_md_est)
+  #   ) %>%
+  #   addLegend(pal = qpal, values = ~gdp_md_est, opacity = 1)
+  
   output$mymap <- renderLeaflet({
     n_to_show = n_flow[grepl(pattern = input$mode, n_flow, ignore.case = TRUE)]
+    qpal = colorBin("RdYlBu", net[[n_to_show]], n = 5)
     leaflet() %>%
       addProviderTiles(providers$OpenStreetMap.BlackAndWhite, options = providerTileOptions(noWrap = TRUE)) %>%
-      addPolylines(data = net, weight = round(net[[n_to_show]] / mean(net[[n_to_show]]) * 5))
+      addPolylines(data = net, weight = round(net[[n_to_show]] / mean(net[[n_to_show]]) * 5), color = ~qpal(net[[n_to_show]])) %>% 
+      addLegend(pal = qpal, values = net[[n_to_show]], opacity = 1)
   })
 }
