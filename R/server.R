2edb92eefd42134f554ef9dc5e191f53313838a2
Robin Lovelace <rob00x@gmail.com>
2019-07-05 19:51:45 +0100

    Add variable line widths

--- R/server.R
+++ R/server.R
@@ -7,17 +7,17 @@
 # Define server logic required to draw a histogram
 shinyAppServer <- function(input, output, session) {
   
+  
   library(leaflet)
   
   net = sf::read_sf("https://github.com/ATFutures/who3/releases/download/0.0.1/net.geojson")
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2, rnorm(40) + 6)
-  }, ignoreNULL = FALSE)
+  n = names(net)
+  n_flow = n[grepl(pattern = "flow_", x = n)]
   
   output$mymap <- renderLeaflet({
+    n_to_show = n_flow[grepl(pattern = input$mode, n_flow, ignore.case = TRUE)]
     leaflet() %>%
       addProviderTiles(providers$OpenStreetMap.BlackAndWhite, options = providerTileOptions(noWrap = TRUE)) %>%
-      addPolylines(data = net)
+      addPolylines(data = net, weight = round(net[[n_to_show]] / mean(net[[n_to_show]]) * 5))
   })
 }
