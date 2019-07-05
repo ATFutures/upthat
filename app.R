0603d8d67c02c47a17257024d062626e614b7b8d
Robin Lovelace <rob00x@gmail.com>
2019-07-05 12:17:02 +0100

    Show net

--- app.R
+++ app.R
@@ -29,7 +29,7 @@ server <- function(input, output, session) {
       addProviderTiles(providers$Stamen.TonerLite,
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addMarkers(data = points())
+      addPolylines(data = net)
   })
 }
 
