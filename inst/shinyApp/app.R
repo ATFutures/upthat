67db25ea74ba02590f3a27aa47712426165607af
Robin Lovelace <rob00x@gmail.com>
2019-07-05 19:16:29 +0100

    Add app files

--- inst/shinyApp/app.R
+++ inst/shinyApp/app.R
@@ -1,1 +1,7 @@
-upthat::runUpthat()
+if(file.exists("R/server.R")) {
+  source("R/server.R")
+  source("R/ui.R")
+  shiny::shinyApp(ui = shinyAppUI, server = shinyAppServer)
+}  else {
+  upthat::runUpthat()
+}
