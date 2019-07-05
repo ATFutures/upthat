573a3d7b453d204dd395ea9358e9a5bf221641d3
Robin Lovelace <rob00x@gmail.com>
2019-07-05 21:46:59 +0100

    Update upthat

--- R/server.R
+++ R/server.R
@@ -6,8 +6,9 @@
 #' @import leaflet
 # Define server logic required to draw a histogram
 shinyAppServer <- function(input, output, session) {
-  if(!file.exists("net.geojson")) download.file("https://github.com/ATFutures/who3/releases/download/0.0.1/net.geojson", "net.geojson")
-  net = sf::read_sf("net.geojson")
+  # if(!file.exists("net.geojson")) download.file("https://github.com/ATFutures/who3/releases/download/0.0.1/net.geojson", "net.geojson")
+  # net = sf::read_sf("net.geojson")
+  net = sf::read_sf("https://github.com/ATFutures/who3/releases/download/0.0.1/net.geojson")
   n = names(net)
   n_flow = n[grepl(pattern = "flow_", x = n)]
   
