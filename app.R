3546560f5ceee4fa7f781a29206f4c468375fd3e
Robin Lovelace <rob00x@gmail.com>
2019-07-05 20:59:53 +0100

    Simplify app.R

--- app.R
+++ app.R
@@ -1,12 +1,4 @@
 library(upthat)
-if(file.exists("R/server.R")) {
-  source("R/server.R")
-  source("R/ui.R")
-  shiny::shinyApp(ui = shinyAppUI, server = shinyAppServer)
-}  else {
-  download.file("https://github.com/atfutures-labs/upthat/archive/master.zip", "master.zip")
-  unzip("master.zip", exdir = ".")
-  source("upthat-master/R/server.R")
-  source("upthat-master/R/ui.R")
-  shinyApp(ui = shinyAppUI, server = shinyAppServer)
-}
+source("R/server.R")
+source("R/ui.R")
+shiny::shinyApp(ui = shinyAppUI, server = shinyAppServer)
