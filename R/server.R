a0a7676e986fe34742d6194adb031abcee7683d2
Robin Lovelace <rob00x@gmail.com>
2019-07-07 15:08:56 +0100

    Revert to online geojson read

--- R/server.R
+++ R/server.R
@@ -13,8 +13,8 @@ shinyAppServer <- function(input, output, session) {
 
   # if(!file.exists("net.geojson")) download.file("https://github.com/ATFutures/who3/releases/download/0.0.1/net.geojson", "net.geojson")
   # net = sf::read_sf("net.geojson")
-  # net = sf::read_sf("https://github.com/ATFutures/who3/releases/download/0.0.1/net.geojson")
-  net = readRDS(system.file("net.Rds", package = "upthat"))
+  net = sf::read_sf("https://github.com/ATFutures/who3/releases/download/0.0.1/net.geojson")
+  # net = readRDS(system.file("net.Rds", package = "upthat"))
   n = names(net)
   n_flow = n[grepl(pattern = "flow_", x = n)]
 
