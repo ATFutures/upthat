21a6649d75b54a40ab8649cb6c512cc771f28b31
Robin Lovelace <rob00x@gmail.com>
2019-07-08 21:28:30 +0100

    Add new modes - test CD - cc @mpadge ; )

--- R/ui.R
+++ R/ui.R
@@ -8,7 +8,7 @@ shinyAppUI <- fluidPage(
   shiny::titlePanel("Urban Planning and Transport Health Assessment Tool (upthat)"),
   column(12, shiny::htmlOutput("app_info")),
   column(width = 3,
-         selectInput("mode", label = "Mode of transport",  choices = c("Walk", "Cycle")),
+         selectInput("mode", label = "Mode of transport",  choices = c("Walk", "Cycle", "Ebike", "Escooter", "Fly")),
          selectInput("pkg", label = "Visualization framework",  choices = c("leaflet", "tmap", "mapdeck")),
          sliderInput("rem", "Re-purposing of car parking spaces (% spaces removed)", min = 0, max = 100, value = 5),
          sliderInput("obs", "Investment (US $ millions):", min = 0, max = 50, value = 0.5, step = 0.1)
