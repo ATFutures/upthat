cd08184ee743a8f500c1632189e9bc89059cf60a
mpadge <mark.padgham@email.com>
2019-11-29 10:48:31 +0100

    change shiny to navbarpAGE

--- R/ui.R
+++ R/ui.R
@@ -4,19 +4,30 @@
 #' @import shiny
 #' @import leaflet
 #' @export
-shinyAppUI = fluidPage(
-  shiny::titlePanel("Urban Planning and Transport Health Assessment Tool (upthat)"),
-  column(12, shiny::htmlOutput("app_info")),
-  column(width = 3,
-         selectInput("city", label = "City",  choices = c("Accra", "Kathmandu", "Bristol", "NYC")),
-         selectInput("mode", label = "Mode of transport",  choices = c("Walk", "Cycle", "Ebike", "Escooter", "Fly")),
-         selectInput("layer", label = "Layer",  choices = c("pedestrian flow", "exposure")),
-         sliderInput("bus", "Number of bus stops added per 1,000", min = 0, max = 10, value = 1),
-         sliderInput("obs", "Investment (US $ millions):", min = 0, max = 50, value = 0.5, step = 0.1)
+shinyAppUI = navbarPage("Urban Planning and Transport Health Assessment Tool (upthat)",
+  tabPanel("Maps",
+    column(12, shiny::htmlOutput("app_info")),
+    column(width = 3,
+           selectInput("city", label = "City",  choices = c("Accra", "Kathmandu", "Bristol", "NYC")),
+           selectInput("mode", label = "Mode of transport",  choices = c("Walk", "Cycle", "Ebike", "Escooter", "Fly")),
+           selectInput("layer", label = "Layer",  choices = c("pedestrian flow", "exposure")),
+           sliderInput("bus", "Number of bus stops added per 1,000", min = 0, max = 10, value = 1),
+           sliderInput("obs", "Investment (US $ millions):", min = 0, max = 50, value = 0.5, step = 0.1)
+    ),
+    column(width = 9,
+           mapdeck::mapdeckOutput ("mymap")
+    ),
+    p(),
+    actionButton("recalc", "Zoom to city extent")
   ),
-  column(width = 9,
-         mapdeck::mapdeckOutput ("mymap")
-  ),
-  p(),
-  actionButton("recalc", "Zoom to city extent")
+  tabPanel("Scenarios",
+    column(width=3,
+           selectInput("city", label = "City",  choices = c("Accra", "Kathmandu", "Bristol", "NYC")),
+           br(),
+           sliderInput("bus", "Number of bus stops added per 1,000", value = 1, min = 1, max = 10)
+    ),
+    column(width=9,
+           selectInput("other", label = "Other",  choices = c("Accra", "Kathmandu", "Bristol", "NYC"))
+    )
+  )
 )
