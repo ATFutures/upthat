98242a2f32977f2436defaa9a85153026bec4395
Robin Lovelace <rob00x@gmail.com>
2019-12-07 16:08:00 +0000

    Close #37

--- R/ui.R
+++ R/ui.R
@@ -9,11 +9,11 @@ shinyAppUI = navbarPage("Urban Planning and Transport Health Assessment Tool (up
     column(12, shiny::htmlOutput("app_info")),
     column(width = 3,
            selectInput("city", label = "City",  choices = c("Accra", "Kathmandu", "Bristol", "NYC")),
-           selectInput("mode", label = "Mode of transport",  choices = c("Walk", "Cycle", "Ebike", "Escooter", "Fly")),
+           # selectInput("mode", label = "Mode of transport",  choices = c("Walk", "Cycle", "Ebike", "Escooter", "Fly")),
            selectInput("layer", label = "Layer",  choices = c("pedestrian flow", "exposure")),
-           selectInput("safety", label = "Safety",  choices = c(TRUE, FALSE), selected = FALSE),
-           sliderInput("bus", "Number of bus stops added per 1,000", min = 0, max = 10, value = 1),
-           sliderInput("obs", "Investment (US $ millions):", min = 0, max = 50, value = 0.5, step = 0.1)
+           selectInput("safety", label = "Safety",  choices = c(TRUE, FALSE), selected = FALSE)
+           # sliderInput("bus", "Number of bus stops added per 1,000", min = 0, max = 10, value = 1),
+           # sliderInput("obs", "Investment (US $ millions):", min = 0, max = 50, value = 0.5, step = 0.1)
     ),
     column(width = 9,
            mapdeck::mapdeckOutput ("mymap")
@@ -22,11 +22,10 @@ shinyAppUI = navbarPage("Urban Planning and Transport Health Assessment Tool (up
     actionButton("recalc", "Zoom to city extent")
   ),
   tabPanel("Health Impacts",
-    column(width=3,
-           selectInput("city_sc", label = "City",  choices = c("Accra", "Kathmandu", "Bristol", "NYC"))
+    column(width = 3,
+           # selectInput("city_sc", label = "City",  choices = c("Accra", "Kathmandu", "Bristol", "NYC"))
     ),
-    column(width=9,
-           plotOutput("plot")
+    column(width = 9, plotOutput("plot")
     )
   )
 )
