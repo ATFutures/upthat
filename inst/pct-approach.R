d26afcab9cf83f78da40e3df012a2e34a187cbfd
Robin Lovelace <rob00x@gmail.com>
2020-03-09 08:03:27 +0000

    update pct approach

--- inst/pct-approach.R
+++ inst/pct-approach.R
@@ -1,3 +1,10 @@
+
+# packages ----------------------------------------------------------------
+
+remotes::install_github("zonebuilders/zonebuilder")
+remotes::install_github("ITSLeeds/od")
+
+library(zonebuilder)
 library(osmdata)
 
 
@@ -29,4 +36,45 @@ if(n_city_name_in_city_data == 1) {
   city = ne_cities[cities_match, ]
 }
 
-region = getbb(place_name = city_name, format_out = "sf_polygon")
+region = osmdata::getbb(place_name = city_name, format_out = "sf_polygon")
+if(is.null(region)) {
+  message("Could not find boundary data from OSM. Getting data from another source.")
+  if(city_name == "accra") {
+    region = sf::read_sf("~/atfutures/who-data/accra/AccraMetroAdmin/AccraMetroAdmin2.shp")
+  }
+  # mapview::mapview(region)
+}
+
+city_centre = tmaptools::geocode_OSM(q = city_name, as.sf = TRUE)
+mapview::mapview(region) + mapview::mapview(city_centre)
+if(!has_zone_data) {
+  city_zones = zonebuilder::zb_zone(x = city_centre, area = region)["label"]
+  plot(city_zones)
+  plot(city_zone_centroids, add = TRUE, col = "grey")
+}
+
+city_zone_centroids$distance_to_centre = as.numeric(sf::st_distance(
+  city_zone_centroids, city_centre)[, 1])
+city_zones$distance_to_centre = city_zone_centroids$distance_to_centre
+plot(city_zones["distance_to_centre"])
+
+
+# generate od data --------------------------------------------------------
+
+od_region = od::points_to_od(city_zone_centroids, interzone_only = TRUE)
+nrow(od_region) # 1560
+od_region = od::od_oneway(od_region)
+nrow(od_region) # 780
+head(od_region)
+od_sf = od::od_to_sf(x = od_region, z = city_zone_centroids)
+plot(od_sf)
+
+
+# generate distance data --------------------------------------------------
+
+od_sf$distance_to_centre = city_zone_centroids$distance_to_centre[
+  match(od_sf$d, city_zone_centroids$label)
+]
+plot(od_sf["distance_to_centre"])
+plot(od_sf["distance_to_centre"][od_sf$distance_to_centre < 5000, ])
+
